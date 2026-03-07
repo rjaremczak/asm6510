fc3c529e828567528281c440a915e9d1edb7546f
Robert Jaremczak <rjaremczak@gmail.com>
2026-03-07 16:48:11 +0100

    migrate to clap - wip

--- src/main.rs
+++ src/main.rs
@@ -3,59 +3,62 @@ extern crate lazy_static;
 
 mod asm6510;
 
+use clap::{Parser,Subcommand};
 use std::path::PathBuf;
-use structopt::StructOpt;
 use asm6510::error::AppError;
 
 fn parse_hex(hex: &str) -> Result<u16, AppError> {
     u16::from_str_radix(hex, 16).map_err(|e| AppError::ParseIntError(String::from(hex), e))
 }
 
-#[derive(Debug, StructOpt)]
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
 #[structopt(about = "My Own 65xx assembler and disassembler")]
-struct CliOpt {
+struct Args {
     /// Execution mode
-    #[structopt(subcommand)]
+    #[command(subcommand)]
     mode: Option<Mode>,
 }
 
-#[derive(Debug, StructOpt)]
+#[derive(Debug, Subcommand)]
 enum Mode {
     /// Assemble source to machine code
     Asm {
         /// Source file path
-        #[structopt(parse(from_os_str))]
+        #[arg(long)]
         src: PathBuf,
         /// Binary file path
-        #[structopt(short = "o", parse(from_os_str))]
+        #[arg(long)]
         bin: Option<PathBuf>,
         /// Dump symbol table
-        #[structopt(short = "s")]
+        #[arg(long)]
         dump_symbols: bool,
     },
     /// Disassemble machine code
-    Dasm {
+    Dis {
         /// Start address
-        #[structopt(parse(try_from_str = parse_hex))]
+        #[arg(long)]
         start_addr: u16,
         /// End address
-        #[structopt(parse(try_from_str = parse_hex))]
+        #[arg(long)]
         end_addr: Option<u16>,
         /// Binary file path
-        #[structopt(parse(from_os_str))]
+        #[arg(long)]
         bin: PathBuf,
     },
 }
 
 fn main() {
-    let cliopt = CliOpt::from_args();
-    let result = match cliopt.mode.unwrap_or() {
-        Mode::Asm {src,bin,dump_symbols} => asm6510::assemble_file(src),
-        Mode::Dasm {start_addr,end_addr,bin} => asm6510::disassemble_file(start_addr, end_addr, bin),
-    };
-    if let Err(apperr) = result {
-        println!("\nerror: {:?}", apperr)
-    }
 
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.mode {
+        Some(Mode::Asm { src, bin, dump_symbols }) => {
+            asm6510::assemble_file(src);
+        },
+        Some(Mode::Dis { start_addr, end_addr, bin }) => {
+            asm6510::disassemble_file(start_addr, end_addr, fpath);
+        },
+        None => println!("\nerror: {:?}", apperr),
+    }
 }
